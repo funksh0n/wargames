4ea0fe749845df7bcdbc133ac1d381303e9ac537
funksh0n <funksh0n@protonmail.ch>
2020-05-17 17:11:17 +0100

    doot

--- makewar.sh
+++ makewar.sh
@@ -6,4 +6,5 @@ if [ -z ${IP+x} ]; then echo "ip not set"; else echo "ip set"; fi
 mkdir /war/$NAME
 touch /war/$NAME/README.md
 sudo nmap --script=vuln -A -vv -oA /war/$NAME/nmap $IP &
-dirbuster -H -l /opt/wordlists/dirbuster/directory-list-2.3-medium.txt -t 200 -r /war/$NAME/ $IP
+dirbuster -H -l /opt/wordlists/dirbuster/directory-list-2.3-medium.txt -t 10 -r /war/$NAME/ $IP &
+gobuster dir -q --noprogress -w /opt/wordlists/dirbuster/directory-list-2.3-medium.txt -t 10 -o /war/$NAME/gobuster -u $IP &
